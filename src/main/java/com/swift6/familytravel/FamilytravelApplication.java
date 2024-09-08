1b0ca0796d44d52a03686e7089bbb7c7548ebc26
yhj216 <yhj216@hanmail.net>
2024-09-09 00:53:26 +0900

    first commit

---
+++ src/main/java/com/swift6/familytravel/FamilytravelApplication.java
@@ -0,0 +1,13 @@
+package com.swift6.familytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilytravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swift6/familytravel/FamilytravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift6.familytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilytravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
