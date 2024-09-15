7c20a48777f54956ad88d4ebb5f8a4b1030d988b
suk3952111 <suk3952111@naver.com>
2024-09-15 17:58:13 +0900

    feat: 개인 프로필 및 그룹 프로필 페이지 분리 및 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,8 +7,8 @@ import styles from './ProfilePage.module.scss';
 
 const cx = classNames.bind(styles);
 
-const NICKNAME = 'JohnDoe';
-const EMAIL = 'johndoe@example.com';
+const NICKNAME = '해바라기';
+const EMAIL = 'johndoe@kakao.com';
 
 export default function ProfilePage() {
   const [toggles, setToggles] = useState({
@@ -25,18 +25,8 @@ export default function ProfilePage() {
 
   return (
     <div className={cx('container')}>
-      <div className={cx('title')}>프로필</div>
-      <div className={cx('image-container')}>
-        <div className={cx('image-container-camera')}>
-          <Image
-            src="/svgs/camera_icon.svg"
-            alt="Camera Icon"
-            width={87}
-            height={87}
-            priority
-          />
-        </div>
-      </div>
+      <div className={cx('title')}>개인 프로필</div>
+      <div className={cx('image-container')} />
       <div className={cx('profile-info-container')}>
         <div className={cx('nickname')}>
           <span>{NICKNAME}</span>
@@ -50,33 +40,6 @@ export default function ProfilePage() {
         </div>
         <div className={cx('email')}>
           <span>{EMAIL}</span>
-        </div>
-      </div>
-      <div className={cx('grid-container')}>
-        <div className={cx('character-container')}>
-          <p>이곳에 캐릭터가 들어갈 예정</p>
-        </div>
-        <div className={cx('settings-container')}>
-          <div className={cx('setting-card', 'anniversary-setting-card')}>
-            <Image
-              src="/svgs/aniversary_icon.svg"
-              alt="Calender Icon"
-              width={19}
-              height={22}
-              priority
-            />
-            <span>기념일 설정</span>
-          </div>
-          <div className={cx('setting-card', 'family-member-setting-card')}>
-            <Image
-              src="/svgs/family_icon.svg"
-              alt="Family Icon"
-              width={20}
-              height={23}
-              priority
-            />
-            <span>가족 구성원 설정</span>
-          </div>
         </div>
       </div>
       <div className={cx('alarm')}>
