8a5eb8e60a74b6fa5aef431cc29edf953b9f1eec
suk3952111 <suk3952111@naver.com>
2024-09-21 00:30:48 +0900

    refactor: import  문 순서 변경

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import classNames from 'classnames/bind';
 import Image from 'next/image';
+import classNames from 'classnames/bind';
 import styles from './GroupPage.module.scss';
 
 const cx = classNames.bind(styles);
