7f6210e6af86cfd6326d8bf777446752cab4bd7c
hyun522 <jhj1004v@naver.com>
2024-09-25 21:36:21 +0900

    FIX : 글자수에 따라 접기 더보기 버튼이 나오도록 구현

--- src/_components/common/MainComment.tsx
+++ src/_components/common/MainComment.tsx
@@ -1,33 +1,54 @@
 'use client';
 
 import classNames from 'classnames/bind';
-import React, { useState } from 'react';
+import React, { useState, MouseEvent, useEffect } from 'react';
 import styles from './MainComment.module.scss';
 
 const cx = classNames.bind(styles);
-export default function MainComment() {
-  const [isExpanded, setIsExpanded] = useState(false); // 상태 관리
 
-  const handleToggle = () => {
-    setIsExpanded(!isExpanded); // 버튼 클릭 시 상태 변경
+interface MainContentProps {
+  text: string;
+}
+
+export default function MainContent({ text }: MainContentProps) {
+  // 더보기 버튼과 접기버튼 번갈아 나오도록
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  // 더보기버튼을 보여줄지 여부
+  const [isClamped, setIsClamped] = useState<boolean>(false);
+  const charLimit = 80;
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    if (isClamped) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    setIsExpanded(!isExpanded);
   };
 
+  useEffect(() => {
+    if (text.length > charLimit) {
+      setIsClamped(true);
+    } else {
+      setIsClamped(false);
+    }
+  }, [text]);
+
   return (
-    // isExpanded가 true일 경우:
-    // <div>는 두 개의 클래스를 가집니다: content expanded
-    <>
-      <div className={cx('content', { expanded: isExpanded })}>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={cx('buttonContainer')}
+    >
+      <div className={cx('ContentStyled', { 'toggle-open': isExpanded })}>
         <p>
-          하늘도 이쁘고 어쩌구 저쩌구 또 가고싶은 여행지다 어쩌구저쩌구
-          어쩌구저ㅉ거하늘도 이쁘고 어쩌구 저쩌구 또 가고싶은 여행지다
-          어쩌구저쩌구어쩌구저ㅉ거하늘도 이쁘고 어쩌구 저쩌구 또 가고싶은
-          여행지다 어쩌구저쩌구어쩌구저ㅉ거하늘도 이쁘고 어쩌구 저쩌구 또
-          가고싶은 여행지다 어쩌구저쩌구어쩌구저ㅉ거
+          {isExpanded || text.length <= charLimit
+            ? text
+            : `${text.slice(0, charLimit)}...`}
         </p>
       </div>
-      <button type="button" className={cx('more')} onClick={handleToggle}>
-        {isExpanded ? '접기' : '더보기'}
-      </button>
-    </>
+      {isClamped && (
+        <p className={cx('toggle')}>{isExpanded ? '접기' : '더보기'}</p>
+      )}
+    </button>
   );
 }
