76e712db97f276fe7e8e1b536b5dbb4ce3517656
hyun522 <jhj1004v@naver.com>
2024-10-13 02:33:28 +0900

    CHORE : type 추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -39,3 +39,32 @@ export interface CheckDestinationListProps {
   checklist: CheckListItem[]; // checklist 배열은 별도의 타입으로 정의 가능
   familyId: number;
 }
+
+export interface FetchGroupParamsType {
+  token?: string;
+  groupId: string;
+}
+
+export interface FamilyImageParams {
+  formData: FormData;
+  token?: string;
+}
+export interface FamilyDetailParams {
+  familyId: string;
+  token?: string;
+}
+
+export interface CreateFamilyRequestParams {
+  token?: string; // 인증에 사용할 JWT 토큰
+  formData: {
+    // 서버로 보낼 데이터 (nickname 등)
+    nickname: string; // 가족 이름 (닉네임)
+  };
+}
+// api/v1/family/all
+export interface FamilyAllItemParams {
+  id: number;
+  name: string;
+  startDate: string;
+  endDate: string;
+}
