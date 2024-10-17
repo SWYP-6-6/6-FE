c5fbe05388cf7c2c4ac41e3beb2ad48169e50713
hyun522 <134246845+hyun522@users.noreply.github.com>
2024-10-18 01:13:59 +0900

    BUG : metadata image 보이도록 수정 (#89)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,61 @@
+// import type { Metadata } from 'next';
+// import '@/styles/base/common.scss';
+// import classNames from 'classnames/bind';
+// import React from 'react';
+// import styles from './layout.module.scss';
+
+// const cx = classNames.bind(styles);
+
+// export const metadata: Metadata = {
+//   title: 'Tripterior',
+//   description: '여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.',
+//   icons: {
+//     icon: '/svgs/favicon.svg',
+//   },
+//   openGraph: {
+//     title: 'Tripterior',
+//     description: '여행의 모든 순간을 특별하게, 트오립테리어와 함께하세요.',
+//     url: 'http://13.209.88.22:3000/',
+//     images: [
+//       {
+//         url: '/svgs/preview.svg',
+//         width: 441,
+//         height: 957,
+//         alt: 'Tripterior Splash Image',
+//       },
+//     ],
+//     type: 'website',
+//     siteName: 'Tripterior',
+//   },
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: 'Tripterior',
+//     description: '여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.',
+//     images: [
+//       {
+//         url: '/svgs/preview.svg',
+//         alt: 'Tripterior Preview Image',
+//       },
+//     ],
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="ko">
+//       <body className={cx('layout')}>{children}</body>
+//     </html>
+//   );
+// }
+
+// description: `${useName}님의 여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.`,
+
 import type { Metadata } from 'next';
+import { META } from '@/constants/metadata';
 import '@/styles/base/common.scss';
 import classNames from 'classnames/bind';
 import React from 'react';
@@ -6,40 +63,34 @@ import styles from './layout.module.scss';
 
 const cx = classNames.bind(styles);
 
+const domain = process.env.NEXT_PUBLIC_DOMAIN || 'http://localhost:3000';
+const ogImageUrl = `${domain}/svgs/preview.svg`;
+
 export const metadata: Metadata = {
-  title: 'Tripterrior',
-  description: '여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.',
+  title: META.title,
+  description: META.description,
   icons: {
-    icon: '/svgs/favicon.svg',
+    icon: META.icon,
   },
   openGraph: {
-    title: 'Tripterrior',
-    description: '여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.',
-    url: 'http://13.209.88.22:3000/',
-    images: [
-      {
-        url: '/svgs/preview.svg',
-        width: 441,
-        height: 957,
-        alt: 'Tripterrior Splash Image',
-      },
-    ],
+    title: META.title,
+    description: META.description,
+    url: META.url,
+    images: {
+      url: ogImageUrl,
+    },
+    siteName: META.siteName,
     type: 'website',
-    siteName: 'Tripterrior',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Tripterrior',
-    description: '여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.',
-    images: [
-      {
-        url: '/svgs/preview.svg',
-        alt: 'Tripterrior Preview Image',
-      },
-    ],
+    title: META.title,
+    description: META.description,
+    images: {
+      url: ogImageUrl,
+    },
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
