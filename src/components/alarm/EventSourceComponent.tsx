c9a3a93aa24081bc2c2a072ce931d4afe47bd418
hyun522 <jhj1004v@naver.com>
2024-10-19 00:34:45 +0900

    FEAT : eventSource 컴포넌트 생성

--- src/components/alarm/EventSourceComponent.tsx
+++ src/components/alarm/EventSourceComponent.tsx
@@ -1,4 +1,4 @@
-// 'use Client';
+// 'use client';
 
 // import React, { useEffect } from 'react';
 // import Cookies from 'js-cookie';
@@ -7,45 +7,49 @@
 // interface EventSourceProps {
 //   setEvent: React.Dispatch<React.SetStateAction<string | null>>; // 실시간 알림 상태 업데이트 함수
 //   setEventSource: React.Dispatch<React.SetStateAction<EventSource | null>>; // EventSource 상태 업데이트 함수
+//   eventSource: EventSource | null; // 외부에서 전달된 EventSource 객체
 // }
 
-// const EventSourceComponent: React.FC<EventSourceProps> = ({
+// function EventSourceComponent({
 //   setEvent,
 //   setEventSource,
-// }) => {
+//   eventSource,
+// }: EventSourceProps) {
 //   const token = Cookies.get('JWT');
+//   console.log(token);
 //   const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN;
 
 //   useEffect(() => {
-//     const EventSource = EventSourcePolyfill || NativeEventSource;
+//     if (!eventSource) {
+//       const EventSource = EventSourcePolyfill || NativeEventSource;
 
-//     try {
-//       const es = new EventSource(`${BASE_URL}/api/v1/notification/connect`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
+//       try {
+//         const es = new EventSource(`${BASE_URL}api/v1/notification/connect`, {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
 
-//       setEventSource(es); // EventSource 객체 저장
+//         setEventSource(es); // EventSource 객체 저장
 
-//       // "notification" 이벤트가 발생할 때마다 실시간 알림 처리
-//       es.addEventListener('notification', (event: MessageEvent) => {
-//         console.log('실시간 알림 데이터:', event.data);
-//         setEvent(event.data); // 상위 컴포넌트에 이벤트 데이터 전달
-//       });
-//     } catch (error) {
-//       console.error('실시간 알림을 설정하는 데 실패했습니다:', error);
+//         es.addEventListener('notification', (event: MessageEvent) => {
+//           console.log('실시간 알림 데이터:', event.data);
+//           setEvent(event.data); // 상위 컴포넌트에 이벤트 데이터 전달
+//         });
+//       } catch (error) {
+//         console.error('실시간 알림을 설정하는 데 실패했습니다:', error);
+//       }
 //     }
 
 //     // 컴포넌트 언마운트 시 EventSource 종료
 //     return () => {
-//       if (es) {
-//         es.close();
+//       if (eventSource) {
+//         eventSource.close();
 //       }
 //     };
-//   }, [setEvent, setEventSource]); // token, setEvent, setEventSource가 변경될 때마다 실행
+//   }, [eventSource, setEvent, setEventSource]); // eventSource와 상태 변경을 주시
 
-//   return null; // UI는 없고 이벤트 핸들링만 수행하는 컴포넌트이므로 아무것도 렌더링하지 않음
-// };
+//   return null; // 이 컴포넌트는 UI가 없으므로 아무것도 렌더링하지 않음
+// }
 
 // export default EventSourceComponent;
