b2f29cdfff6d13ea8711285585918112f98ae190
hyun522 <134246845+hyun522@users.noreply.github.com>
2024-10-19 02:46:03 +0900

    Revert "Create automatic.yml (#99)" (#102)
    This reverts commit 7fbb95af76d450afe17172348daf94cf8342ad9d.

--- src/constants/metadata.ts
+++
@@ -1,8 +0,0 @@
-export const META = {
-  title: 'Tripterior',
-  description: '여행의 모든 순간을 특별하게, 트립테리어와 함께하세요.',
-  url: 'http://13.209.88.22:3000/',
-  icon: '/svgs/favicon.svg',
-  ogImage: 'http://13.209.88.22:3000/opengraph-image.png',
-  siteName: 'Tripterior',
-} as const;
