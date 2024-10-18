2c8d528045ac317a458654c366aef2ab7f5622f2
hyun522 <jhj1004v@naver.com>
2024-10-18 14:38:59 +0900

    DESIGN : 모바일 화면에서 버튼 보이게 수정

---
+++ src/app/alarm/page.tsx
@@ -0,0 +1,14 @@
+// import AlarmList from '@/components/alarm/AlarmList';
+import Header from '@/components/common/Header';
+import React from 'react';
+
+export default function page() {
+  return (
+    <div>
+      <Header isShowProfile={false} isShowButton>
+        알림
+      </Header>
+      {/* <AlarmList /> */}
+    </div>
+  );
+}
---
+++ src/components/alarm/AlarmList.tsx
@@ -0,0 +1,34 @@
+// 'use client';
+
+// import classNames from 'classnames/bind';
+// import React from 'react';
+// import Image from 'next/image';
+// import styles from './AlarmList.module.scss';
+
+// const cx = classNames.bind(styles);
+
+// const AlarmList = [
+//   {
+//     useProfile:
+//     userName: 한쥬
+//     text:
+//   }
+// ]
+
+// export default function AlarmList() {
+//   return (
+//     <ul className={cx('container')}>
+//       <li>
+//         <div className >
+//           <Image />
+//         </div>
+//         <div>
+//           <p>
+//             <strong>{userName}</strong>
+//             <span>{text}</span>
+//           </p>
+//         </div>
+//       </li>
+//     </ul>
+//   );
+// }
