d0a75eb94896dc14283ab3f11e2b3b3a7ae1f861
hyun522 <jhj1004v@naver.com>
2024-10-19 03:14:07 +0900

    CHORE : route 적용

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,17 +1,13 @@
-// app/api/auth/route.ts
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const jwt = searchParams.get('jwt');
 
   if (jwt) {
-    const cookieStore = cookies();
-    cookieStore.set('JWT', jwt, { maxAge: 60 * 60 * 24, httpOnly: false });
-
-    // After setting the cookie, redirect to the homepage or another destination
-    return NextResponse.redirect('/');
+    const response = NextResponse.redirect('/');
+    response.cookies.set('JWT', jwt, { maxAge: 60 * 60 * 24, httpOnly: false });
+    return response;
   }
 
   return NextResponse.json({ error: 'JWT token is missing' }, { status: 400 });
