215cab6333558311d55df6d8cdfec3560ce71977
suk3952111 <suk3952111@naver.com>
2024-10-19 04:00:58 +0900

    refactor: 코드

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const jwt = searchParams.get('jwt');
 
   if (jwt) {
-    const response = NextResponse.redirect('https://tripterrior.vercel.app/');
-    response.cookies.set('JWT', jwt, { maxAge: 60 * 60 * 24, httpOnly: false });
-    return response;
+    const cookieStore = cookies();
+    cookieStore.set('JWT', jwt, { maxAge: 60 * 60 * 24, httpOnly: false });
+
+    // After setting the cookie, redirect to the homepage or another destination
+    return NextResponse.redirect('http://13.209.88.22:3000/');
   }
 
   return NextResponse.json({ error: 'JWT token is missing' }, { status: 400 });
